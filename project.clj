f82000c7a09bbfb943f55175c463b8b309e0c15f
Neale Swinnerton <neale@sw1nn.com>
2021-02-04 18:28:19 +0000

    Fix the repository urls.

--- project.clj
+++ project.clj
@@ -16,6 +16,6 @@
   :plugins [[s3-wagon-private "1.3.4"]]
 
   ;; this isn't actually no-auth, it uses the standard AWS auth mechanisms
-  :repositories [["releases" {:url "s3p://griffin-maven-development" :no-auth true}]
-                 ["snapshots" {:url "s3p://griffin-maven-development" :no-auth true}]]
+  :repositories [["releases" {:url "s3p://griffin-maven-dev-eu-west-2/release" :no-auth true}]
+                 ["snapshots" {:url "s3p://griffin-maven-dev-eu-west-2/snapshot" :no-auth true}]]
   )
