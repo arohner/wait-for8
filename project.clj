25d7a57561f0b0fd8b1c318475f2c58cd974564c
Neale Swinnerton <neale@sw1nn.com>
2021-02-04 15:26:01 +0000

    Change project id and release repository.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject arohner/wait-for8 "1.0.6-SNAPSHOT"
+(defproject sh.griffin/wait-for8 "1.0.6-SNAPSHOT"
   :description "wait-for, using java8 time instead of joda"
+  :url "https://github.com/griffinbank/wait-for8"
   :license {:name "Eclipse Public License 1.0"
             :url "https://www.eclipse.org/legal/epl-v10.html"
             :year 2017
@@ -11,4 +12,10 @@
                                   [org.clojure/test.check "1.1.0"]]
                    :exclusions [org.clojure/clojurescript
                                 com.cemerick/clojurescript.test]}}
-  :deploy-repositories [["releases" :clojars]])
+
+  :plugins [[s3-wagon-private "1.3.4"]]
+
+  ;; this isn't actually no-auth, it uses the standard AWS auth mechanisms
+  :repositories [["releases" {:url "s3p://griffin-maven-development" :no-auth true}]
+                 ["snapshots" {:url "s3p://griffin-maven-development" :no-auth true}]]
+  )
